6f31988b77b6594ae4c10a0e897b762545cd6e83
Rashad Alakbarov <rashad.alakbarov.777@gmail.com>
2025-09-15 00:14:51 +0400

    create new project and adeded Navbar section to website

---
+++ src/datas/info.js
@@ -0,0 +1,9 @@
+import logo from "../assets/logo.svg";
+import profile_pic from "../assets/default/profile_pic.png";
+import dropdown_icon from "../assets/icons/dropdown_icon.svg";
+
+export const assets = {
+  logo,
+  profile_pic,
+  dropdown_icon,
+};
---
+++ src/pages/MyAppointments.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MyAppointments = () => {
+  return (
+    <div>
+      <h1>MyAppointments</h1>
+    </div>
+  );
+};
+
+export default MyAppointments;
